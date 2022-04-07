cf3b4378d3639cd93b827985552d8aa30c28bd36
fathan <fathan.pranaya@gmail.com>
2022-04-07 13:43:46 +0900

    added email counter instead of filename

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import shutil
 
 if __name__ == '__main__':
     # variable to check same sender email address
-    existing_files = []
+    existing_email = []
 
     # list all files in current directory
     files = os.listdir('input')
@@ -27,16 +27,21 @@ if __name__ == '__main__':
             # get format of file
             fmt = re.search(r'\.(\w+)$', file)
 
-            new_filename = email.group(0) + '.' + fmt.group(1)
             # add counter on the new_filename if the file already exists
-            if new_filename in existing_files:
-                new_filename = email.group(0) + '_' + str(existing_files.count(new_filename)) + '.' + fmt.group(1)
+            new_email = email.group(0)
+            if new_email in existing_email:
+                new_filename = new_email + '_' + str(existing_email.count(new_email)) + '.' + fmt.group(1)
+            else:
+                new_filename = new_email + '.' + fmt.group(1)
 
             # debugging
-            print(f'{file[-30:]} --> {new_filename}')
+            # print(f'{cnt} {file[-30:]} --> {new_filename}')
 
             # copy file to output folder
-            shutil.copy(f'input/{file}', f'output/{new_filename}')
-            existing_files.append(new_filename)
+            res = shutil.copy(f'input/{file}', f'output/{new_filename}')
+            print(f'copying file: {res}')
+            existing_email.append(new_email)
+        else:
+            print(f'cannot rename {file}')
 
     print(f'Renaming success: {len(files)} files')
